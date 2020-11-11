fe1e3776b7dd8d8ca3017dae5c4f22d2f0ff916e
jeffreycnhub <15157650@qq.com>
2020-11-11 10:22:38 +0800

    20201111-1

--- src/test/java/com/junit/demo/Junit5Demo_4_1_Parallel.java
+++ src/test/java/com/junit/demo/Junit5Demo_4_1_Parallel.java
@@ -1,5 +1,22 @@
 package com.junit.demo;
 
+
+import com.util.Calculator;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
 public class Junit5Demo_4_1_Parallel {
+    @RepeatedTest(10)
+    @Execution(ExecutionMode.CONCURRENT)
+    public void contTest() throws InterruptedException{
+        int result = Calculator.count(1);
+        System.out.println(result);
+    }
+    @RepeatedTest(10)
+    public void countSynTest() throws InterruptedException{
+        int result = Calculator.count(1);
+        System.out.println(result);
+    }
 
 }
